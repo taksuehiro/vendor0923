6056b0365a49573a163ae117a0b3a19fd6c5fc76
taksuehiro <tsuehiro0706@yahoo.jp>
2025-10-01 19:34:21 +0900

    change52

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,9 @@ const isCI = !!process.env.CI || !!process.env.AMPLIFY_BRANCH;
 const nextConfig = {
   eslint: { ignoreDuringBuilds: isCI },
   typescript: { ignoreBuildErrors: isCI },
+  // ↓ これを追加
+  trailingSlash: false,
+  skipTrailingSlashRedirect: true,
 };
 
 module.exports = nextConfig;
